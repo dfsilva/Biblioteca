0c57ef8d8e5c1183f67621ca7e2ac1e40f8407ae
Diego <diegosiuniube@gmail.com>
2015-11-09 14:48:23 -0200

    alteracoes

--- src/br/aedu/anhanghera/poo/biblioteca/Principal.java
+++ src/br/aedu/anhanghera/poo/biblioteca/Principal.java
@@ -1,16 +1,8 @@
 package br.aedu.anhanghera.poo.biblioteca;
 
-import br.aedu.anhanghera.poo.biblioteca.bd.FabricaConexao;
-import br.aedu.anhanghera.poo.biblioteca.dominio.Aluno;
 import br.aedu.anhanghera.poo.biblioteca.dominio.Livro;
-import br.aedu.anhanghera.poo.biblioteca.excecao.LoginException;
-import br.aedu.anhanghera.poo.biblioteca.excecao.ReservaLivroException;
 import br.aedu.anhanghera.poo.biblioteca.negocio.BibliotecaNegocio;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Principal {
 
@@ -18,17 +10,17 @@ public class Principal {
 
     public static void main(String[] args) {
 
-      bibliotecaNegocio.popularLivros();
-      
-      List<Livro> livros = bibliotecaNegocio.listarLivros();
-      
-      System.out.println("Existe "+livros.size()
-              +" cadastrados no sistema");
-      
-      for(int i =0; i< livros.size(); i++){
-          System.out.println(livros.get(i).getIsbn());
-      }
-      
+        bibliotecaNegocio.popularLivros();
+
+        List<Livro> livros = bibliotecaNegocio.listarLivros();
+
+        System.out.println("Existe " + livros.size()
+                + " cadastrados no sistema");
+
+        for (int i = 0; i < livros.size(); i++) {
+            System.out.println(livros.get(i).getIsbn());
+        }
+
     }
 
 }
