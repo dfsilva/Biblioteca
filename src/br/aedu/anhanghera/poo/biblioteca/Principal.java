33bda7a7f51ae3284c151ee734f1df62df10dc29
Diego Silva <diegosiuniube@gmail.com>
2015-11-11 09:21:27 -0200

    altreacoes

--- src/br/aedu/anhanghera/poo/biblioteca/Principal.java
+++ src/br/aedu/anhanghera/poo/biblioteca/Principal.java
@@ -6,6 +6,7 @@ import br.aedu.anhanghera.poo.biblioteca.dominio.Livro;
 import br.aedu.anhanghera.poo.biblioteca.excecao.LoginException;
 import br.aedu.anhanghera.poo.biblioteca.excecao.ReservaLivroException;
 import br.aedu.anhanghera.poo.biblioteca.negocio.BibliotecaNegocio;
+import br.aedu.anhanghera.poo.biblioteca.telas.TelaPrincipal;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -20,14 +21,11 @@ public class Principal {
 
       bibliotecaNegocio.popularLivros();
       
-      List<Livro> livros = bibliotecaNegocio.listarLivros();
-      
-      System.out.println("Existe "+livros.size()
-              +" cadastrados no sistema");
-      
-      for(int i =0; i< livros.size(); i++){
-          System.out.println(livros.get(i).getIsbn());
-      }
+      java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaPrincipal().setVisible(true);
+            }
+        });
       
     }
 
---
+++ src/br/aedu/anhanghera/poo/biblioteca/telas/modelo/LivrosTableModel.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.aedu.anhanghera.poo.biblioteca.telas.modelo;
+
+import br.aedu.anhanghera.poo.biblioteca.dominio.Livro;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author diego
+ */
+public class LivrosTableModel extends AbstractTableModel{
+
+    private List<Livro> livros;
+    private String[] colunas;
+
+    public LivrosTableModel(List<Livro> livros) {
+        this.livros = livros;
+        this.colunas = new String[]{"ID",
+        "Nome", "ISBN"};
+    }
+    
+    @Override
+    public int getRowCount() {
+       return this.livros.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int indiceLinha, 
+            int indiceColuna) {
+        Livro livro = this.livros.get(indiceLinha);
+        switch(indiceColuna){
+            case 0: return livro.getId();
+            case 1: return livro.getNome();
+            case 2: return livro.getIsbn();
+        }
+        return null;
+    }
+    
+}
